e9ae2d0c75ee89449d15d6a994ae3d8e085abf04
Angel Fernando <smn@cmain>
2020-04-19 19:58:07 -0700

    ingreso de cuentas

--- main.cpp
+++ main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include <mysql.h>
+#include <string.h>
 
 using namespace std;
 
+void out(bool o);
+void fail(char s[]);
+
+MYSQL *con;
+
 int main(){
-  MYSQL *con=mysql_init(NULL);
+  char name[10];
+  char c[]="insert into users(name) values('%s')";
+  char *q;
+
+  con=mysql_init(NULL);
   if(!mysql_real_connect(con,"localhost","root","tabladelfin","counts",0,NULL,0)) cout << mysql_error(con) << endl;
   else cout << "Conexion exitosa a db counts\n";
 
+  cout << "ingresar nombre: ";
+  cin.getline(name,10,'\n');
+
+  q=new char[strlen(c)+sizeof(name)-sizeof(int)];
+  sprintf(q,c,name);
+
+  mysql_query(con,q);
   
-  return 0;
+  out(0);
+}
+
+void out(bool o){
+  mysql_close(con);
+  exit(o);
+}
+void fail(char s[]){
+  cout << mysql_error(con) << endl;
+  cout << s << endl;
+  out(1);
 }
