8eacada0deb9222ce52a10daa796c4bb11526a32
Angel Fernando <smn@cmain>
2020-06-16 17:54:12 -0700

    inicio de sesion y verificacion de archivo

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@ int main(){
   Table data(10,40);
   MyCon mc("localhost","root","tabladelfin","counts");
   char result[10];
+  FILE *f;
 
   cout << "ingresar nombre: ";
   cin.getline(data.t[0],10,'\n');
   fflush(stdin);
   cout << "ingresar contraseña: ";
   cin.getline(data.t[1],8,'\n');
+  cout << "ingresar archivo: ";
+  cin.getline(data.t[2],20,'\n');
   
   
   if(mc.queryd(c,data)) mc.fail();
@@ -21,10 +24,18 @@ int main(){
   cout << "id: " << (strcmp(result,"")==0 ? "error de acceso" : result) << endl;
 
   if(strcmp(result,"")!=0){
+    /*
     cout << "tabla de usuarios\n";
     mc.query("select * from user");
     mc.outtable(data);
     for(int i=0;i<data.y;i++) cout << data.t[i] << endl;
+    */
+    f=fopen(data.t[2],"rb");
+    if(f==NULL) cout << "Archivo inexistente" << endl;
+    else{
+      cout << "Archivo existente" << endl;
+      fclose(f);
+    }
   }
   
   mc.close();
